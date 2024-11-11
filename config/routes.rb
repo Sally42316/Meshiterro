2bf517953f77f9d28fea350f1224f289f21159a1
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2024-11-11 13:04:45 +0000

    [Add]アソシエーションを作成/PostImages コントローラを作成/

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   root to: "homes#about"
   get 'homes/about', to: 'homes#about', as: 'about'
 
+  resources :post_images, only: [:new, :index, :show]
+
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
