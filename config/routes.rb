16363e61da97b75e95b8f83fe4d7be96ca4538bc
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2024-11-12 05:55:34 +0000

    [Add]投稿画面の追加/登録機能の追加/投稿したPostImageを一覧表示する一覧画面を作成/

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: "homes#about"
   get 'homes/about', to: 'homes#about', as: 'about'
 
-  resources :post_images, only: [:new, :index, :show]
+  post 'post_images' => 'post_images#create'
+
+  resources :post_images, only: [:new, :create, :index, :show]
 
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
