542b9e386d5d7290b96d9409d1c5b1b5d19194bf
EC2 Default User <ec2-user@ip-172-31-42-54.ap-northeast-1.compute.internal>
2024-11-13 09:02:06 +0000

    [Add]18章【SNSらしい機能を追加しよう(1) - コメント機能】済み

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get "homes/about" => "homes#about", as: "about"
-  resources :post_images, only: [:new, :create, :index, :show, :destroy, :edit]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
+
+
   resources :users, only: [:show, :edit, :update]
   post 'post_images' => 'post_images#create'
 
